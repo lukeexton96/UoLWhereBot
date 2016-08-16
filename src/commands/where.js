32756ffb3c3fe4463f5382879689fc9d74692d67
Luke Exton <lukeexton@Lukes-MacBook-Pro.local>
2016-08-16 08:16:51 +0100

    <LE> Example response based off pre-defined user input.

--- src/commands/where.js
+++ src/commands/where.js
@@ -13,14 +13,24 @@ const msgDefaults = {
 const handler = (payload, res) => {
 
     var requestText = payload.text.toLowerCase()
-
-    var reponseText = "Helllooooooo"
-
     var attachments = null;
+    var reponseText
+
+    if(requestText.slice(0,3)=="is ") requestText = requestText.substring(3)
+    if(requestText.slice(-1)=="?") requestText = reponseText.substring(0, requestText.length - 1)
+
+    switch(requestText){
+      case 'CMP':
+        responseText = "This is a response to CMP"
+        break;
+
+      default:
+        responseText = "I don't recognise that command."
+    }
 
     let msg = _.defaults({
       channel: payload.channel_name,
-      text: requestText,
+      text: reponseText,
       attachments: attachments
     }, msgDefaults)
 
